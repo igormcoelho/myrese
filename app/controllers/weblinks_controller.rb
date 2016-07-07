47b4bbc672c17c3d141ad2e28925bc8b3460dbbc
Igor Machado Coelho <igor.machado@gmail.com>
2016-07-07 23:15:44 +0000

    improved Weblink index and edit pages

--- app/controllers/weblinks_controller.rb
+++ app/controllers/weblinks_controller.rb
@@ -4,7 +4,10 @@ class WeblinksController < ApplicationController
   # GET /weblinks
   # GET /weblinks.json
   def index
-    @weblinks = Weblink.all
+    #@weblinks = Weblink.all
+    @weblinks  = Weblink.joins(:infohash_users).joins(:infohash).where("infohash_users.user_id = ?", current_user.id)
+    @weblinks += Weblink.joins(:infohash).where("infohashes.user_id = ?", current_user.id)
+    @weblinks.uniq!
   end
 
   # GET /weblinks/1
@@ -14,17 +17,25 @@ class WeblinksController < ApplicationController
 
   # GET /weblinks/new
   def new
-    @weblink = Weblink.new
+    @weblink          = Weblink.new
+    @infohash         = Infohash.new
+    @weblink.infohash = @infohash
   end
 
   # GET /weblinks/1/edit
   def edit
+   @infohash = @weblink.infohash
   end
 
   # POST /weblinks
   # POST /weblinks.json
   def create
-    @weblink = Weblink.new(weblink_params)
+    @infohash = Infohash.new(infohash_params)
+    @weblink  = @infohash.build_weblink(weblink_params)
+
+    @infohash.user     = current_user
+    @infohash.htype_id = Weblink::HTYPE
+    @infohash.code     = "link" + Project.count.to_s
 
     respond_to do |format|
       if @weblink.save
@@ -41,6 +52,7 @@ class WeblinksController < ApplicationController
   # PATCH/PUT /weblinks/1.json
   def update
     respond_to do |format|
+      @weblink.infohash.assign_attributes(infohash_params)
       if @weblink.update(weblink_params)
         format.html { redirect_to @weblink, notice: 'Weblink was successfully updated.' }
         format.json { render :show, status: :ok, location: @weblink }
@@ -54,7 +66,7 @@ class WeblinksController < ApplicationController
   # DELETE /weblinks/1
   # DELETE /weblinks/1.json
   def destroy
-    @weblink.destroy
+    @weblink.infohash.destroy
     respond_to do |format|
       format.html { redirect_to weblinks_url, notice: 'Weblink was successfully destroyed.' }
       format.json { head :no_content }
@@ -69,6 +81,10 @@ class WeblinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def weblink_params
-      params.require(:weblink).permit(:infohash_id, :link)
+      params.require(:weblink).permit(:link)
+    end
+    
+    def infohash_params
+      params.require(:weblink).permit(:gtitle, :gdescription, :visibility_id)
     end
 end
--- app/models/weblink.rb
+++ app/models/weblink.rb
@@ -1,9 +1,16 @@
 class Weblink < ActiveRecord::Base
   HTYPE = 6
   
-  belongs_to :infohash
+  belongs_to :infohash   , autosave: true
   
-  delegate :user, to: :infohash
+  delegate :user         , to: :infohash # belongs_to through (doesn't exist!)
+  delegate :members      , to: :infohash
+  has_many :infohash_users  , through: :infohash
+  
+  delegate :gtitle       , to: :infohash
+  delegate :gdescription , to: :infohash
+  delegate :visibility   , to: :infohash
+  delegate :visibility_id, to: :infohash
   
   validates_presence_of :link
 end
