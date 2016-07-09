b775b26441156a8e9f0935c0deb477085bb00791
Igor Machado Coelho <igor.machado@gmail.com>
2016-07-09 05:12:08 +0000

    userref working in posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,29 @@ module ApplicationHelper
       gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
       "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=mm"
     end
+    
+    def instanceName
+      return "http://myrese.org"
+    end
+    
+    def instanceUserPath(userref)
+      return link_to(userref, instanceName+"/u/"+userref[1..-1])
+    end
+    
+    def getGoodLinks(text)
+      sanitize findReplaceUserRef(text).html_safe, :tags => %w(a)
+    end
+    
+    def verifyList(list, value)
+      return list.include?value
+    end
+    
+    def findReplaceUserRef(text)
+      uall = User.all
+      userlist = []
+      uall.each do |u|
+        userlist << "@"+u.username
+      end
+      return text.gsub(/[@][a-zA-Z][\w]+/) { |m| verifyList(userlist,m) ? instanceUserPath(m).html_safe : m } 
+    end
 end
