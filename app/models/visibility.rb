9709e8434b990660e0aa2dbad8b2faabf13608df
Igor Machado Coelho <igor.machado@gmail.com>
2016-08-02 13:45:40 +0000

    unique visibility types

--- app/models/visibility.rb
+++ app/models/visibility.rb
@@ -1,6 +1,7 @@
 class Visibility < ActiveRecord::Base
     ##self.inheritance_column = nil # fix for field 'type', which should be reserved... now it is 'vtype', no problem!
     validates_presence_of :vtype
+    validates_uniqueness_of :vtype
     
     #VCODES = { vprivate: 1, vinstance: 2, vpublic: 3 } # TODO: REMOVE vtype field and htypes table!
     
