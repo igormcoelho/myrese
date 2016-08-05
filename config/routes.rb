3e094f6cf40dd135df7fc11d2404784f140a67d2
Igor Machado Coelho <igor.machado@gmail.com>
2016-08-05 03:29:29 +0000

    created table connecting any infohash to unregistered members

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :infohash_members
   root to: "mainpage#index"
   
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718020954) do
+ActiveRecord::Schema.define(version: 20160805032840) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "title"
@@ -128,6 +128,16 @@ ActiveRecord::Schema.define(version: 20160718020954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "infohash_members", force: :cascade do |t|
+    t.integer  "infohash_id"
+    t.string   "author"
+    t.string   "identification"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "infohash_members", ["infohash_id"], name: "index_infohash_members_on_infohash_id"
 
   create_table "infohash_users", force: :cascade do |t|
     t.integer  "infohash_id"
