41fc0759d484b4fbe7dd0480be53cbd3f71ec11b
Igor Machado Coelho <igor.machado@gmail.com>
2017-12-20 21:36:06 +0000

    added config to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   
   # begin mailer configuration
-  config.action_mailer.default_url_options = { host: 'myrese.com', port: 80 }
-
-  config.action_mailer.delivery_method = :smtp
+  # mailer configuration!
+  config.action_mailer.default_url_options = { host: 'myrese.com' }
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.mailgun_settings = {
+		api_key: ENV['PROD_MAIL_MAILGUN_TOKEN'],
+		domain: ENV['PROD_MAIL_MAILGUN_HOST']
+}
+#  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
